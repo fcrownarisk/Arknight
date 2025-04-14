e992f1fbdd2f769c441fdbc1dcc19ce196747a2b
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-14 17:49:23 +0800

    Update RETURN.c

--- RETURN.c
+++ RETURN.c
@@ -1,9 +1,9 @@
 #include "abcdefgh.ts"
 void main(int argc,char * argv[]){
-   return  "a + b + c + d"<=>"e + f + g + h"
-   return  "a - b - c - d">=<"e - f - g - h"
-   return  "a * b * c * d"<=>"e * f * g * h"
-   return  "a - b - c - d">=<"e - f - g - h"
-   return  "a / b / c / d"<=>"e / f / g / h"
-   return  "a % b % c % d">=<"e % f % g % h"
+   return  "a + b + c + d"<=>"e + f + g + h";
+   return  "a - b - c - d">=<"e - f - g - h";
+   return  "a * b * c * d"<=>"e * f * g * h";
+   return  "a - b - c - d">=<"e - f - g - h";
+   return  "a / b / c / d"<=>"e / f / g / h";
+   return  "a % b % c % d">=<"e % f % g % h";
 }
