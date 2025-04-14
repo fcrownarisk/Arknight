efa27b06c0a85e18647d3e30e1d97ed530808d6e
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-14 17:46:01 +0800

    Create RETURN.c

---
+++ RETURN.c
@@ -0,0 +1,8 @@
+void main(int argc,char * argv[]){
+   return  "a + b + c + d"<=>"e + f + g + h"
+   return  "a - b - c - d">=<"e - f - g - h"
+   return  "a * b * c * d"<=>"e * f * g * h"
+   return  "a - b - c - d">=<"e - f - g - h"
+   return  "a / b / c / d"<=>"e / f / g / h"
+   return  "a % b % c % d">=<"e % f % g % h"
+}
