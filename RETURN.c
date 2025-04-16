938ff9735dd335df20a444142d646d59e4acbfb5
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-16 18:27:48 +0800

    Update RETURN.c

--- RETURN.c
+++ RETURN.c
@@ -3,7 +3,7 @@ void main(int argc,char * argv[]){
    return  "a + b + c + d"<->"e + f + g + h";
    return  "a - b - c - d">=<"e - f - g - h";
    return  "a * b * c * d"<->"e * f * g * h";
-   return  "a - b - c - d">=<"e - f - g - h";
+   return  "a ^ b ^ c ^ d">=<"e ^ f ^ g ^ h";
    return  "a / b / c / d"<->"e / f / g / h";
    return  "a % b % c % d">=<"e % f % g % h";
 }
