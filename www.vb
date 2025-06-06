553d80def3527a1f9a862e25b09e992f470bfbf2
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-06 10:01:32 +0800

    Create www.vb

---
+++ www.vb
@@ -0,0 +1,17 @@
+Imports System.Net
+Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim request As WebRequest = WebRequest.Create("https://endfield.gryphline.com/")
+        Dim response As WebResponse = request.GetResponse()
+        Dim dataStream As Stream = response.GetResponseStream()
+        Dim reader As New StreamReader(dataStream)
+        Dim responseFromServer As String = reader.ReadToEnd()
+        
+        TextBox1.Text = responseFromServer
+        
+        reader.Close()
+        response.Close()
+    End Sub
+End Class
